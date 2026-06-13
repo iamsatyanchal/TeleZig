f6ef1170ea7c1e7faa400608202f99259e6c830f
iamsatyanchal <quickhalewebtech@gmail.com>
2026-06-13 23:02:49 +0530

    intiated the main.zig file buddy

---
+++ main.zig
@@ -0,0 +1,9 @@
+const std = @import("std");
+const show = std.debug.print;
+const memory = std.mem;
+const heap = std.heap;
+
+pub fn main() !void {
+    // idk zig man so this is one of my first commit so yeahh heppy hepyyyy
+    show("this is a test", .{});
+}
